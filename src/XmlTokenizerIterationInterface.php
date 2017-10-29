67daad06d109cf991c35a82f87c48bf0b0f97af2
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-29 17:14:47 +0100

    Added `XmlTokenizerIterationInterface`

---
+++ src/XmlTokenizerIterationInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Parser\Tokenizer;
+
+/**
+ * An iteration of an XML tokenizer.
+ *
+ * @since [*next-version*]
+ */
+interface XmlTokenizerIterationInterface extends
+        TokenizerIterationInterface,
+        XmlTokenAwareInterface
+{
+}
